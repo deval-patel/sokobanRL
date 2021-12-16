16c38ab5306e49cf8b71e551826b2c013664988d
Deval <devalrocket@gmail.com>
2021-12-16 08:02:16 -0500

    Add reward system comments

--- gym_498_sokoban/RewardSystem.py
+++ gym_498_sokoban/RewardSystem.py
@@ -6,20 +6,25 @@ class RewardSystem:
 
     @staticmethod
     def get_reward_for_box_on_target():
+        """ for moving a box onto a target """
         return 8.0
 
     @staticmethod
     def get_reward_for_box_off_target():
+        """ for moving a box off the target """
         return -8.0
 
     @staticmethod
     def get_reward_for_invalid_move():
+        """ for taking an action which results in an invalid move """
         return -3.0
 
     @staticmethod
     def get_reward_for_victory():
+        """ for winning the level """
         return 30.0
 
     @staticmethod
     def get_reward_for_loss():
+        """ for losing the level """
         return -30.0
